475268bb18ec2a4a64d78ebe0608972bf5aa28ed
TopHat <buffinthemuppinbuisness@gmail.com>
2024-07-12 14:55:44 -0700

    Updated game.py to include movement, and have the positions set in a variable and made the game also render the sprites

--- game.py
+++ game.py
@@ -11,14 +11,55 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.playerplaceholder = pygame.image.load('data/sprites/player-placeholder.png')
+
+        self.playerplaceholder_pos = [500, 350]
+        self.movement = [False, False, False, False]
     def run(self):
         while True:
-            self.screen.blit(self.playerplaceholder, (500, 350))
+            self.screen.fill((0, 0, 0))
+            self.playerplaceholder_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.playerplaceholder_pos[0] += (self.movement[3] - self.movement[2]) * 5
+            self.screen.blit(self.playerplaceholder, self.playerplaceholder_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                        print('up')
+                    elif event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                        print('down')
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                        print('up')
+                    elif event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+                        print('down')
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                        print('up')
+                    elif event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                        print('down')
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[2] = True
+                        print('left')
+                    elif event.key == pygame.K_RIGHT:
+                        self.movement[3] = True
+                        print('right')
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[2] = False
+                        print('left')
+                    elif event.key == pygame.K_RIGHT:
+                        self.movement[3] = False
+                        print('right')
             pygame.display.update()
             self.clock.tick(60)
 
